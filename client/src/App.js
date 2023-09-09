dd1d891b2319e1071bc5bb8507d5e272a8e03a86
Neeraj Choubisa <neerajchoubisa876@gmail.com>
2023-09-09 18:24:31 +0530

    Update

--- client/src/App.js
+++ client/src/App.js
@@ -1,84 +1,47 @@
-import { Typography, Container, Box, Paper } from "@mui/material";
-import { useEtherspotAddresses } from "@etherspot/transaction-kit";
-import { useEffect, useState } from "react";
-import MintTktTokens from "./components/MintTktTokens/MintTktTokens";
-import StakeTktTokens from "./components/StakeTktTokens/StakeTktTokens";
-
+import {
+  EtherspotBatches,
+  EtherspotBatch,
+  EtherspotTransaction,
+  useEtherspotAddresses,
+  useEtherspotTransactions,
+} from "@etherspot/transaction-kit";
+import { useState } from "react";
 export default function App() {
-  const [hasSentMintTx, setHasSentMintTx] = useState(false);
-  const [hasSentStakingTx, setHasSentStakingTx] = useState(false);
+  const { estimate, send } = useEtherspotTransactions();
   const etherspotAddresses = useEtherspotAddresses();
-  const [goerliSmartWalletAddress, setGoerliSmartWalletAddress] = useState(
-    false
+
+  const [address, setAddress] = useState(
+    "0xcfa038455b54714821f291814071161c9870B891"
   );
-
-  useEffect(() => {
-    console.log("etherspotAddresses", etherspotAddresses);
-
-    const fetchGoerliSmartWallet = () => {
-      if (etherspotAddresses.length && !goerliSmartWalletAddress) {
-        const goerliNetwork = etherspotAddresses.find(
-          (network) => network.chainName === "mumbai"
-        );
-        console.log("goerliNetwork", goerliNetwork);
-        console.log(goerliNetwork);
-        setGoerliSmartWalletAddress(goerliNetwork.address);
-      }
-    };
-
-    fetchGoerliSmartWallet();
-  }, [etherspotAddresses, goerliSmartWalletAddress]);
-
-  const onSentStakingTransactionReceiver = (e) => {
-    console.log("Sent Staking Transaction:", e);
-    setHasSentStakingTx(true);
-  };
-
-  const onSentMintTransactionReceiver = (e) => {
-    console.log("Sent Mint Transaction:", e);
-    setHasSentMintTx(true);
-  };
-
+  const [amount, setAmount] = useState("0.001");
 
   return (
-    <Container>
-      <Typography variant="h3">Transaction Kit</Typography>
-      <Typography variant="h4">Staking Example</Typography>
-
-      <Box m={2} />
-
-      {goerliSmartWalletAddress ? (
-        <MintTktTokens
-          hasSentMintTx={hasSentMintTx}
-          onSentMintTransactionReceiver={onSentMintTransactionReceiver}
-          goerliSmartWalletAddress={goerliSmartWalletAddress}
-        />
-      ) : null}
-
-      <Box m={2} />
-
-      {hasSentMintTx ? (
-        <StakeTktTokens
-          hasSentMintTx={hasSentMintTx}
-          hasSentStakingTx={hasSentStakingTx}
-          onSentStakingTransactionReceiver={onSentStakingTransactionReceiver}
-          goerliSmartWalletAddress={goerliSmartWalletAddress}
-        />
-      ) : null}
-
-      <Box m={4} />
-
-      <Paper sx={{ p: 2 }}>
-        <Typography textAlign={"center"} variant="subtitle2">
-          Etherspot Smart Wallet address: {goerliSmartWalletAddress}
-        </Typography>
-        <Typography textAlign={"center"} variant="subtitle2">
-          TKT Token Contract: {"0x2A9bb3fB4FBF8e536b9a6cBEbA33C4CD18369EaF"}
-        </Typography>
-        <Typography textAlign={"center"} variant="subtitle2">
-          TKT Staking Contract: {"0x0493b9a21dE42546B2E3687Da683D0B7B6ec2180"}
-        </Typography>
-      </Paper>
-    </Container>
+    <EtherspotBatches>
+      <EtherspotBatch>
+        <EtherspotTransaction to={address} value={amount}>
+         
+          {etherspotAddresses.map((etherspotAddressObject, i) => (
+            <div key={i}>
+              <p>Blockchain Name: {etherspotAddressObject.chainName}</p>
+              <p>Blockchain ID:{etherspotAddressObject.chainId}</p>
+              <p>Address: {etherspotAddressObject.address}</p>
+            </div>
+          ))}
+          <input
+            type="text"
+            value={address}
+            onChange={(event) => setAddress(event.target.value)}
+          />
+          <input
+            type="text"
+            value={amount}
+            onChange={(event) => setAmount(event.target.value)}
+          />
+          <hr />
+          <button onClick={() => estimate()}>Estimate</button>
+          <button onClick={() => send()}>Send</button>
+        </EtherspotTransaction>
+      </EtherspotBatch>
+    </EtherspotBatches>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,23 +1,19 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
-import { EtherspotTransactionKit } from "@etherspot/transaction-kit";
-import * as ethers from "ethers";
-
 import App from "./App";
+import { EtherspotTransactionKit } from "@etherspot/transaction-kit";
+import { ethers } from "ethers";
+
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
-
 const randomWallet = ethers.Wallet.createRandom();
 const providerWallet = new ethers.Wallet(randomWallet.privateKey);
 
-console.log(providerWallet);
-
 root.render(
-  <StrictMode>
-    <EtherspotTransactionKit provider={providerWallet} chainId={80001}>
-      <App />
-    </EtherspotTransactionKit>
-  </StrictMode>
+  <EtherspotTransactionKit
+    provider={providerWallet} /* The random wallet we created above */
+    chainId={80001} /* Polygon Testnet - Mumbai */
+  >
+    <App />
+  </EtherspotTransactionKit>
 );
